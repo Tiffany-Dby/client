e882641d07a30c1cf15f55570dc523b8b9d37a93
Tiffany-Dby <tiffany.dby@gmail.com>
2025-03-18 16:25:35 +0100

    add(dark mode): theme context and toggle mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./shared/assets/styles/index.css";
 import App from "./App.tsx";
+import { ThemeProvider } from "./lib/context/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
